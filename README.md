8dbd6603756d73ea43704710701aad91ce86b6ff
Alex Feel <alexfeel@protonmail.com>
2025-05-05 10:07:57 +0300

    feat: add initial version

---
+++ README.md
@@ -0,0 +1,14 @@
+# Streamlit Copy-to-Clipboard Button Component • Example App
+
+This repository contains a tiny Streamlit app that demonstrates how to use the **st‑copy** component.
+
+> **Component code** lives in the main project:
+> <https://github.com/alex-feel/st-copy>
+
+## Quick start
+
+```bash
+pip install -r requirements.txt
+
+streamlit run app.py
+```
---
+++ app.py
@@ -0,0 +1,120 @@
+import streamlit as st
+import importlib.metadata as meta
+
+from st_copy import copy_button
+
+# Base settings
+st.set_page_config(page_title="Copy Button Demo • Chat API")
+
+# Sidebar
+with st.sidebar:
+    st.title("st‑copy • demo")
+
+    st.markdown(
+        "🔗 **Source code**: "
+        "[alex‑feel/st‑copy](https://github.com/alex-feel/st-copy)"
+    )
+
+    version = meta.version("st-copy")
+    st.markdown(
+        "[![PyPI](https://img.shields.io/pypi/v/st-copy.svg)]"
+        "(https://pypi.org/project/st-copy/)"
+    )
+
+    st.markdown(
+        "[![Python Version](https://img.shields.io/python/required-version-toml?tomlFilePath=https%3A%2F%2Fraw.githubusercontent.com%2Falex-feel%2Fst-copy%2Frefs%2Fheads%2Fmain%2Fpyproject.toml)]"
+        "(https://github.com/alex-feel/st-copy/blob/main/pyproject.toml)"
+    )
+
+    st.markdown(
+        "[![GitHub License](https://img.shields.io/github/license/alex-feel/st-copy)]"
+        "(https://github.com/alex-feel/st-copy/blob/main/LICENSE)"
+    )
+
+    install_cmd = f"pip install st-copy=={version}"
+    st.code(install_cmd, language="bash")
+    copy_button(install_cmd, key="sidebar‑install")
+
+    st.markdown("---")
+    st.markdown("Made with 💚 for Streamlit Community")
+
+# Main app
+st.title('Copy Button Demo • Chat API')
+
+msg_1 = '''Use the following code to add a copy button:
+
+```python
+from st_copy import copy_button
+
+msg = 'some message'
+
+copy_button(msg)
+```'''
+with st.chat_message('assistant'):
+    st.markdown(f'**Assistant:** {msg_1}')
+    copy_button(msg_1)
+
+msg_2 = '''Use the following code to add a copy button with a custom tooltip and/or label:
+
+```python
+from st_copy import copy_button
+
+msg = 'some message'
+
+copy_button(
+    msg,
+    tooltip='✨ Special tooltip!',
+    copied_label='Check my tooltip!',
+)
+```'''
+with st.chat_message('assistant'):
+    st.markdown(f'**Assistant:** {msg_2}')
+    copy_button(
+        msg_2,
+        tooltip='✨ Special tooltip!',
+        copied_label='Check my tooltip!',
+    )
+
+msg_3 = '''Use the following code to add the button style Streamlit uses in code blocks:
+
+```python
+from st_copy import copy_button
+
+msg = 'some message'
+
+copy_button(
+    msg,
+    icon='st',
+)
+```'''
+with st.chat_message('assistant'):
+    st.markdown(f'**Assistant:** {msg_3}')
+    copy_button(
+        msg_3,
+        icon='st',
+    )
+
+msg_4 = '''Use all parameters together:
+
+```python
+from st_copy import copy_button
+
+msg = 'some message'
+
+copy_button(
+    msg,
+    icon='material_symbols',  # default, use 'st' as alternative
+    tooltip='Any tooltip text',  # defaults to 'Copy'
+    copied_label='Custom "Copied!" text',  # defaults to 'Copied!'
+    key='Any key',  # If omitted, a random key will be generated
+)
+```'''
+with st.chat_message('assistant'):
+    st.markdown(f'**Assistant:** {msg_4}')
+    copy_button(
+        msg_4,
+        icon='material_symbols',
+        tooltip='Any tooltip text',
+        copied_label='Custom "Copied!" text',
+        key='Any key',
+    )
